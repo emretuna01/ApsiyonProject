0e28398862c6e687e654b4be2fa23c4faf1a8854
Emre Tuna <emretuna@outlook.com>
2021-06-23 00:55:41 +0300

    entities created

---
+++ ApsiyonProject.Domain/App/Entities/Building.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApsiyonProject.Domain.App.Entities
+{
+    public class Building
+    {
+        public string BlockName { get; set; }
+        public BuildingStatus BuildingStatus { get; set; }
+        public BuildingType BuildingType{ get; set; }
+        
+    }
+}
---
+++ ApsiyonProject.Domain/App/Entities/Floor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApsiyonProject.Domain.App.Entities
+{
+    public class Floor
+    {
+        public string FloorNumber { get; set; }
+    }
+}
