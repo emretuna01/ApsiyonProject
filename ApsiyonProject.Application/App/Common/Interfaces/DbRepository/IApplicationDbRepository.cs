7f65566a94dac1eded98b1d0a37005001b652e6d
Emre Tuna <emretuna@outlook.com>
2021-06-24 22:01:34 +0300

    SqlServer added

--- ApsiyonProject.Application/App/Common/Interfaces/DbRepository/IApplicationDbRepository.cs
+++ ApsiyonProject.Application/App/Common/Interfaces/DbRepository/IApplicationDbRepository.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using ApsiyonProject.Domain.App.Common;
 
 namespace ApsiyonProject.Application.App.Common.Interfaces.DbRepository
 {
-    public interface IApplicationDbRepository
+    public interface IApplicationDbRepository<T> where T : BaseEntitiy
     {
+        public Task<T> GetTypeAsync();
+        public Task<List<T>> GetListAsync();
+        public Task<T> GetWhereAsync(Expression<Func<T,bool>> expression);        
+        public Task<int> AddTypeAsync(T type);
+        public Task<int> AddRangeAsync(List<T> typeList);
+        public Task<int> TruncateAsync();
+
+
     }
 }
---
+++ ApsiyonProject.Presentation/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ApsiyonProject.Presentation.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
